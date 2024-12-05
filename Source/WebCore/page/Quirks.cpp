40a4e10bdac4a4795f4213ca18239c89e0b6fd7b
Karl Dubost <k_dubost@apple.com>
2024-12-05 22:37:43 +0900

    Remove Quirk needsIPadMiniUserAgent for a series of domains https://bugs.webkit.org/show_bug.cgi?id=284103 rdar://140974340
    Reviewed by NOBODY (OOPS!).
    
    This list of domains had User Agent overrides to give an iPad Mini
    UA instead of the usual desktop User Agent. This was done initially
    to give most of the time UI compatible with touch events when the
    website were unable to manage a device with a desktop UA and touch
    interface. This list of domains has been tested and the UA override
    is not required anymore.
    
    * rdar://59480381 fidelity.com
    * rdar://51885199 hsbc.com
    * rdar://51355671 tving.com
    * rdar://51355671 live.iqiyi.com
    * rdar://51355671 video.sina.com.cn
    * rdar://51355671 video.tudou.com
    * rdar://51355671 v.china.com.cn
    * rdar://49124313 trello.com
    * rdar://51463649 ted.com
    * rdar://67823088 nhl.com
    * rdar://50839844 jsfiddle.net
    
    * Source/WebCore/page/Quirks.cpp:
    (WebCore::Quirks::needsIPadMiniUserAgent):

--- Source/WebCore/page/Quirks.cpp
+++ Source/WebCore/page/Quirks.cpp
@@ -1793,34 +1793,10 @@ bool Quirks::needsIPadMiniUserAgent(const URL& url)
     if (host == "tv.kakao.com"_s || host.endsWith(".tv.kakao.com"_s))
         return true;
 
-    if (host == "tving.com"_s || host.endsWith(".tving.com"_s))
-        return true;
-
-    if (host == "live.iqiyi.com"_s || host.endsWith(".live.iqiyi.com"_s))
-        return true;
-
-    if (host == "jsfiddle.net"_s || host.endsWith(".jsfiddle.net"_s))
-        return true;
-
-    if (host == "video.sina.com.cn"_s || host.endsWith(".video.sina.com.cn"_s))
-        return true;
-
     if (host == "huya.com"_s || host.endsWith(".huya.com"_s))
         return true;
 
-    if (host == "video.tudou.com"_s || host.endsWith(".video.tudou.com"_s))
-        return true;
-
     if (host == "cctv.com"_s || host.endsWith(".cctv.com"_s))
-        return true;
-
-    if (host == "v.china.com.cn"_s)
-        return true;
-
-    if (host == "trello.com"_s || host.endsWith(".trello.com"_s))
-        return true;
-
-    if (host == "ted.com"_s || host.endsWith(".ted.com"_s))
         return true;
 
     if (host.contains("hsbc."_s)) {
@@ -1842,18 +1818,9 @@ bool Quirks::needsIPadMiniUserAgent(const URL& url)
             return true;
         if (host == "hsbc.com.ph"_s || host.endsWith(".hsbc.com.ph"_s))
             return true;
-        if (host == "hsbc.com"_s || host.endsWith(".hsbc.com"_s))
-            return true;
         if (host == "hsbc.com.cn"_s || host.endsWith(".hsbc.com.cn"_s))
             return true;
     }
-
-    if (host == "nhl.com"_s || host.endsWith(".nhl.com"_s))
-        return true;
-
-    // FIXME: Remove this quirk when <rdar://problem/59480381> is complete.
-    if (host == "fidelity.com"_s || host.endsWith(".fidelity.com"_s))
-        return true;
 
     // FIXME: Remove this quirk when <rdar://problem/61733101> is complete.
     if (host == "roblox.com"_s || host.endsWith(".roblox.com"_s))
